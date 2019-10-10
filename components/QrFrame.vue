c2af6290d25572255159f919d1da62df82bf53fe
nredjap <hinanias@gmail.com>
2019-10-10 11:49:07 +0900

    update QRが出るようになった

---
+++ components/QrFrame.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    koko-ni-qr-takusan:{{ label }}
+    <qrcode-vue :value="codeText" />
+  </div>
+</template>
+
+<script>
+import QrcodeVue from 'qrcode.vue'
+export default {
+  components: {
+    QrcodeVue
+  },
+  props: {
+    label: {
+      type: String,
+      default: 'undefined'
+    },
+    codeText: {
+      type: String,
+      default: undefined
+    }
+  }
+}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,30 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">QRprint</h1>
-      <h2 class="subtitle">Print QRcodes from json text</h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
+      <div class="field">
+        <div class="control">
+          <input
+            v-model="qrtext"
+            class="input"
+            type="text"
+            placeholder="Normal input"
+          />
+        </div>
       </div>
+      <qr-frame :label="aaa" :code-text="bbb"></qr-frame>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import QrFrame from '~/components/QrFrame.vue'
 
 export default {
   components: {
-    Logo
+    QrFrame
+  },
+  data() {
+    return { qrtext: '' }
   }
 }
 </script>
@@ -37,27 +37,5 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
